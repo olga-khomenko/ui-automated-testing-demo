67831c641e065b1eab0c1275a9b85b474ba8d181
Olga Khomenko <homenko.olga@gmail.com>
2023-03-15 23:02:16 -0400

    Move page selectors to classes

--- infrastructure/page_objects/inventory_page.py
+++ infrastructure/page_objects/inventory_page.py
@@ -1,24 +1,28 @@
+from dataclasses import dataclass
 from playwright.sync_api import Page
 
 
-menu = 'button[id="react-burger-menu-btn"]'
-close_menu ='button[id="react-burger-cross-btn"]'
-logout = 'a[id="logout_sidebar_link"]'
+@dataclass
+class Selectors:
+    menu: str = 'button[id="react-burger-menu-btn"]'
+    close_menu: str ='button[id="react-burger-cross-btn"]'
+    logout: str = 'a[id="logout_sidebar_link"]'
 
 
 class InventoryPage:
 
     def __init__(self, page: Page):
         self.page = page
+        self.s = Selectors()
 
     @property
     def open_menu(self):
-        self.page.locator(menu).click()
-        self.page.locator(close_menu).wait_for()
+        self.page.locator(self.s.menu).click()
+        self.page.locator(self.s.close_menu).wait_for()
 
     @property
     def do_logout(self):
-        from ..page_objects.login_page import LoginPage
+        from .login_page import LoginPage
         self.open_menu
-        self.page.locator(logout).click()
+        self.page.locator(self.s.logout).click()
         return LoginPage(self.page)
--- infrastructure/page_objects/login_page.py
+++ infrastructure/page_objects/login_page.py
@@ -1,22 +1,27 @@
+from dataclasses import dataclass
 from typing import Self
 from playwright.sync_api import Page
-from ..page_objects.inventory_page import InventoryPage
+from .inventory_page import InventoryPage
 
-input_username = 'input[data-test="username"]'
-input_password = 'input[data-test="password"]'
-login_button = 'input[data-test="login-button"]'
-error_box = 'h3[data-test="error"]'
+
+@dataclass
+class Selectors:
+    input_username: str = 'input[data-test="username"]'
+    input_password: str = 'input[data-test="password"]'
+    login_button: str = 'input[data-test="login-button"]'
+    error_box: str = 'h3[data-test="error"]'
 
 
 class LoginPage:
 
     def __init__(self, page: Page):
         self.page = page
+        self.s = Selectors()
 
     def do_login(self, username: str, password: str) -> Self | InventoryPage:
-        self.page.locator(input_username).fill(username)
-        self.page.locator(input_password).fill(password)
-        self.page.locator(login_button).click()
+        self.page.locator(self.s.input_username).fill(username)
+        self.page.locator(self.s.input_password).fill(password)
+        self.page.locator(self.s.login_button).click()
         if self.page.url == 'https://www.saucedemo.com/inventory.html':
             return InventoryPage(self.page)
         return LoginPage(self.page)
