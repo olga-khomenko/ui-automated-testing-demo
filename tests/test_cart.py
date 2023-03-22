1d6cd8c32545c145309f78cf59880ad155c493cb
Olga Khomenko <homenko.olga@gmail.com>
2023-03-22 19:34:18 -0400

    Test remove item from cart

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -3,7 +3,7 @@
 class TestCart:
 
     def test_checkout_cart(self, inventory_page):
-        inventory_page.add_to_cart(item='t-shirt-(red)')
+        inventory_page.add_to_cart(item='test.allthethings()-t-shirt-(red)')
         cart = inventory_page.open_cart
         checkout_page = cart.do_checkout()
         checkout_page.enter_customer_data('John', 'Doe', '25480')
@@ -12,3 +12,13 @@ class TestCart:
 
         assert checkout_page.title_value_is('Checkout: Complete!')
         assert checkout_page.header_value_is('Thank you for your order!')
+
+    def test_remove_from_cart(self, inventory_page):
+        inventory_page.add_to_cart(item='sauce-labs-bike-light')
+        cart_page = inventory_page.open_cart
+
+        assert cart_page.displays_item('Sauce Labs Bike Light')
+
+        cart_page.remove_item('sauce-labs-bike-light')
+
+        assert not cart_page.displays_item('Sauce Labs Bike Light')
